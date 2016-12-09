49716b751ee7343565c8d815b195e8a39ee47096
Jared Flater <flaterj1@dev-intel16.i>
2016-12-09 15:55:07 -0500

    best hit from blast with id cutoff

---
+++ best_hit_blast_idcut.py
@@ -0,0 +1,15 @@
+import sys
+
+d = {}
+cutoff = sys.argv[2]
+for line in open(sys.argv[1]):
+    data = line.rstrip().split('\t')
+    query = data[0]
+    hit = data[1]
+    prcntid = float(data[2])
+    if d.has_key(query): 
+        continue
+    else:
+        if prcntid > cutoff:
+            d[query] = hit
+            print line,
